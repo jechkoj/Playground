fb6feb1ccaf953bc8fb68f2f56b9abb41a48a3f0
Zhechko Zhechev <jechkoj@gmail.com>
2015-03-22 13:46:47 +0200

    added <=> (sgn) function

--- Playground.playground/section-1.swift
+++ Playground.playground/section-1.swift
@@ -1,3 +1,26 @@
-import UIKit
+/**
+Compares two numbers lhs and rhs.
 
-var str = "Hello, playground"
+:param: lhs numerical value.
+:param: rhs numerical value.
+
+:returns: 1 whether lhs > rhs, -1 whether lhs < rhs; otherwise 0.
+*/
+infix operator <=> {
+    precedence 0
+    associativity none
+}
+
+func <=> <T: Comparable> (lhs: T, rhs: T) -> Int {
+    if lhs > rhs {
+        return 1
+    } else if lhs < rhs {
+        return -1
+    }
+    
+    return 0
+}
+
+3 <=> 4
+0 <=> -5
+0 <=> 0
